7918ed95fd32dd8935da6aa67eccf9a56e5d2ef7
Stephen A. Imhoff <clockwork-muse@outlook.com>
2015-12-21 11:42:20 +0900

    Update extracted wait handle tests.

--- src/System.Threading.Tasks/tests/IAsyncWaitHandleTests.cs
+++ src/System.Threading.Tasks/tests/IAsyncWaitHandleTests.cs
@@ -8,26 +8,41 @@ namespace System.Threading.Tasks.Tests
     public static class IAsyncWaitHandleTests
     {
         [Fact]
-        public static void RunAsyncWaitHandleTests()
+        public static void RunningTask_Timeout()
         {
-            // Start a task, but make sure that it does not complete
-            ManualResetEvent mre = new ManualResetEvent(false);
-            Task t1 = Task.Factory.StartNew(() => { mre.WaitOne(); });
+            using (ManualResetEvent mre = new ManualResetEvent(false))
+            {
+                // Start a task, but make sure that it does not complete
+                Task running = new TaskFactory().StartNew(() => { mre.WaitOne(); });
 
-            // Make sure that waiting on an uncompleted Task's AsyncWaitHandle does not succeed
-            WaitHandle wh = ((IAsyncResult)t1).AsyncWaitHandle;
-            Assert.False(wh.WaitOne(0), "RunAsyncWaitHandleTests:  Did not expect wait on uncompleted Task's AsyncWaitHandle to succeed");
+                WaitHandle handle = ((IAsyncResult)running).AsyncWaitHandle;
 
-            // Make sure that waiting on a completed Task's AsyncWaitHandle succeeds
-            mre.Set();
-            Assert.True(wh.WaitOne(), "RunAsyncWaitHandleTests:  Expected wait on completed Task's AsyncWaitHandle to succeed");
+                // Make sure that waiting on an uncompleted Task's AsyncWaitHandle does not succeed
+                Assert.False(handle.WaitOne(0));
+                Assert.False(handle.WaitOne(TimeSpan.Zero));
+                Assert.False(handle.WaitOne(1));
+                Assert.False(handle.WaitOne(TimeSpan.FromMilliseconds(1)));
 
-            // To complete coverage for CompletedEvent_get, we need to grab a fresh AsyncWaitHandle from
-            // an already-completed Task.
-            t1 = Task.Factory.StartNew(() => { });
-            t1.Wait();
-            wh = ((IAsyncResult)t1).AsyncWaitHandle;
-            Assert.True(wh.WaitOne(0), "RunAsyncWaitHandleTests:  Expected wait on AsyncWaitHandle from completed Task to succeed");
+                // Make sure that waiting on a completed Task's AsyncWaitHandle (eventually) succeeds
+                mre.Set();
+                Assert.True(handle.WaitOne());
+            }
+        }
+
+        [Fact]
+        public static void CompletedTask_Wait()
+        {
+            // Waiting on an already-completed task should succeed immediately.
+            Task completed = new TaskFactory().StartNew(() => { });
+            completed.Wait();
+
+            WaitHandle handle = ((IAsyncResult)completed).AsyncWaitHandle;
+
+            Assert.True(handle.WaitOne());
+            Assert.True(handle.WaitOne(0));
+            Assert.True(handle.WaitOne(TimeSpan.Zero));
+            Assert.True(handle.WaitOne(1));
+            Assert.True(handle.WaitOne(TimeSpan.FromMilliseconds(1)));
         }
     }
 }
