0b61b04ac1857e28004ae97625fe151e6297df55
Stephen A. Imhoff <clockwork-muse@outlook.com>
2016-03-01 10:49:47 +0900

    Add basic tests for TaskSchedulerException

---
+++ src/System.Threading.Tasks/tests/TaskScheduler/TaskSchedulerExceptionTests.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Xunit;
+
+namespace System.Threading.Tasks.Tests
+{
+    public static class TaskSchedulerExceptionTests
+    {
+        [Fact]
+        public static void TaskSchedulerException_Constructor_Default()
+        {
+            TaskSchedulerException tse = new TaskSchedulerException();
+
+            Assert.NotNull(tse);
+            Assert.Null(tse.InnerException);
+        }
+
+        [Fact]
+        public static void TaskSchedulerException_Constructor_Message()
+        {
+            string message = "message";
+
+            TaskSchedulerException tse = new TaskSchedulerException(message);
+
+            Assert.NotNull(tse);
+            Assert.Null(tse.InnerException);
+            Assert.Equal(message, tse.Message);
+        }
+
+        [Fact]
+        public static void TaskSchedulerException_Constructor_InnerException()
+        {
+            DeliberateTestException inner = new DeliberateTestException();
+
+            TaskSchedulerException tse = new TaskSchedulerException(inner);
+
+            Assert.NotNull(tse);
+            Assert.Equal(inner, tse.InnerException);
+        }
+
+        [Fact]
+        public static void TaskSchedulerException_Constructor_Message_InnerException()
+        {
+            string message = "message";
+            DeliberateTestException inner = new DeliberateTestException();
+
+            TaskSchedulerException tse = new TaskSchedulerException(message, inner);
+
+            Assert.NotNull(tse);
+            Assert.Equal(inner, tse.InnerException);
+            Assert.Equal(message, tse.Message);
+        }
+    }
+}
