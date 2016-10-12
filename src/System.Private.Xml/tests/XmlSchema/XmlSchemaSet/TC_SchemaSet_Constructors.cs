e6ff41af2ab8094d20eb464e72cccfed356a04ed
Stephen A. Imhoff <clockwork-muse@outlook.com>
2016-10-11 17:22:55 -0700

    Constructors

--- src/System.Private.Xml/tests/XmlSchema/XmlSchemaSet/TC_SchemaSet_Constructors.cs
+++ src/System.Private.Xml/tests/XmlSchema/XmlSchemaSet/TC_SchemaSet_Constructors.cs
@@ -2,57 +2,39 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Xml.Schema;
 using Xunit;
 using Xunit.Abstractions;
-using System.Xml.Schema;
 
 namespace System.Xml.Tests
 {
     //[TestCase(Name = "TC_SchemaSet_Constructors", Desc = "", Priority = 0)]
-    public class TC_SchemaSet_Constructors : TC_SchemaSetBase
+    public static class TC_SchemaSet_Constructors
     {
-        private ITestOutputHelper _output;
-
-        public TC_SchemaSet_Constructors(ITestOutputHelper output)
-        {
-            _output = output;
-        }
-
-
         //-----------------------------------------------------------------------------------
         [Fact]
         //[Variation(Desc = "v1 - Default constructor", Priority = 1)]
-        public void v1()
+        public static void v1()
         {
             XmlSchemaSet sc = new XmlSchemaSet();
-            return;
         }
 
         //-----------------------------------------------------------------------------------
         [Fact]
         //[Variation(Desc = "v2 - XmlSchemaSet(XmlNameTable = 0)")]
-        public void v2()
+        public static void v2()
         {
-            try
-            {
-                XmlSchemaSet sc = new XmlSchemaSet(null);
-            }
-            catch (ArgumentNullException)
-            {
-                // GLOBALIZATION
-                return;
-            }
-            Assert.True(false);
+            // GLOBALIZATION
+            Assert.Throws<ArgumentNullException>("nameTable", () => new XmlSchemaSet(null));
         }
 
         [Fact]
         //[Variation(Desc = "v3 - XmlDataSourceResolver(XmlNameTable = valid) check back")]
-        public void v3()
+        public static void v3()
         {
             NameTable NT = new NameTable();
             XmlSchemaSet sc = new XmlSchemaSet(NT);
-            Assert.Equal(sc.NameTable, NT);
-            return;
+            Assert.Same(sc.NameTable, NT);
         }
     }
 }
