c61c5472fa2de16273bec142ec9e08b6fd77c029
Stephen A. Imhoff <clockwork-muse@outlook.com>
2016-10-13 17:33:23 -0700

    IsCompiled

--- src/System.Private.Xml/tests/XmlSchema/XmlSchemaSet/TC_SchemaSet_IsCompiled.cs
+++ src/System.Private.Xml/tests/XmlSchema/XmlSchemaSet/TC_SchemaSet_IsCompiled.cs
@@ -2,65 +2,50 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Xunit;
-using Xunit.Abstractions;
 using System.Xml.Schema;
+using Xunit;
 
 namespace System.Xml.Tests
 {
     //[TestCase(Name = "TC_SchemaSet_IsCompiled", Desc = "")]
-    public class TC_SchemaSet_IsCompiled : TC_SchemaSetBase
+    public static class TC_SchemaSet_IsCompiled
     {
-        private ITestOutputHelper _output;
-
-        public TC_SchemaSet_IsCompiled(ITestOutputHelper output)
-        {
-            _output = output;
-        }
-
-
         //-----------------------------------------------------------------------------------
         //[Variation(Desc = "v1 - IsCompiled on empty collection", Priority = 0)]
-        [InlineData()]
-        [Theory]
-        public void v1()
+        [Fact]
+        public static void v1()
         {
             XmlSchemaSet sc = new XmlSchemaSet();
-            CError.Compare(sc.IsCompiled, false, "IsCompiled");
-            return;
+            Assert.False(sc.IsCompiled);
         }
 
         //-----------------------------------------------------------------------------------
         //[Variation(Desc = "v2 - IsCompiled, add one, Compile, IsCompiled, add one IsCompiled", Priority = 0)]
-        [InlineData()]
-        [Theory]
-        public void v2()
+        [Fact]
+        public static void v2()
         {
             XmlSchemaSet sc = new XmlSchemaSet();
             sc.Add("xsdauthor1", TestData._XsdNoNs);
 
-            CError.Compare(sc.IsCompiled, false, "IsCompiled after first add");
+            Assert.False(sc.IsCompiled);
 
             sc.Compile();
 
-            CError.Compare(sc.IsCompiled, true, "IsCompiled after compile");
+            Assert.True(sc.IsCompiled);
 
             sc.Add("xsdauthor", TestData._XsdAuthor);
 
-            CError.Compare(sc.IsCompiled, false, "IsCompiled after seciond add");
+            Assert.False(sc.IsCompiled);
 
             sc.Compile();
 
-            CError.Compare(sc.IsCompiled, true, "IsCompiled after second compile");
-
-            return;
+            Assert.True(sc.IsCompiled);
         }
 
         //-----------------------------------------------------------------------------------
         //[Variation(Desc = "v3 - Add two, Compile, remove one, IsCompiled")]
-        [InlineData()]
-        [Theory]
-        public void v3()
+        [Fact]
+        public static void v3()
         {
             XmlSchemaSet sc = new XmlSchemaSet();
             sc.Add("xsdauthor1", TestData._XsdNoNs);
@@ -68,9 +53,7 @@ namespace System.Xml.Tests
             sc.Compile();
             sc.Remove(Schema1);
 
-            CError.Compare(sc.IsCompiled, false, "IsCompiled after compiled and remove");
-
-            return;
+            Assert.False(sc.IsCompiled);
         }
     }
 }
