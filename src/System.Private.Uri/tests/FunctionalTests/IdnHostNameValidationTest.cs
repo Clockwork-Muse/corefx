aa01bea99409cbe65f7ad9309828751353c23d00
Stephen A. Imhoff <clockwork-muse@outlook.com>
2016-10-20 12:22:26 -0700

    Switch to data driven

--- src/System.Private.Uri/tests/FunctionalTests/IdnHostNameValidationTest.cs
+++ src/System.Private.Uri/tests/FunctionalTests/IdnHostNameValidationTest.cs
@@ -2,123 +2,63 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using Xunit;
 
 namespace System.PrivateUri.Tests
 {
-    public class IdnHostNameValidationTest
+    public static class IdnHostNameValidationTest
     {
         [Fact]
-        public void IdnHost_Call()
+        public static void IdnHost_Call()
         {
             Uri u = new Uri("http://someHost\u1234.com");
             Assert.Equal("xn--somehost-vk7a.com", u.IdnHost);
         }
 
         [Fact]
-        public void IdnHost_Call_ThrowsException()
+        public static void IdnHost_Call_ThrowsException()
         {
-            Assert.Throws<InvalidOperationException>(() =>
-            {
-                Uri u = new Uri("/test/test2/file.tst", UriKind.Relative);
-                string s = u.IdnHost;
-            });
+            Uri u = new Uri("/test/test2/file.tst", UriKind.Relative);
+            Assert.Throws<InvalidOperationException>(() => u.IdnHost);
         }
 
         [Fact]
-        public void IdnHost_UnknownFqdn_Success()
+        public static void IdnHost_Internal_Call_ThrowsException()
         {
-            CommonSchemesTest("unknown");
+            Uri u = new Uri("/test/test2/file.tst", UriKind.Relative);
+            Assert.Throws<InvalidOperationException>(() => u.DnsSafeHost);
         }
 
-        [Fact]
-        public void IdnHost_HttpFqdn_Success()
-        {
-            CommonSchemesTest("http");
-        }
-
-        [Fact]
-        public void IdnHost_FtpUnicodeFqdn_Success()
-        {
-            CommonSchemesTest("ftp");
-        }
-
-        [Fact]
-        public void IdnHost_FileFqdn_Success()
-        {
-            CommonSchemesTest("file");
-        }
-
-        [Fact]
-        public void IdnHost_NetPipeFqdn_Success()
-        {
-            CommonSchemesTest("net.pipe");
-        }
-
-        [Fact]
-        public void IdnHost_NetTcpFqdn_Success()
-        {
-            CommonSchemesTest("net.tcp");
-        }
-
-        [Fact]
-        public void IdnHost_VSMacrosFqdn_Success()
-        {
-            CommonSchemesTest("vsmacros");
-        }
-
-        [Fact]
-        public void IdnHost_GopherFqdn_Success()
-        {
-            CommonSchemesTest("gopher");
-        }
-
-        [Fact]
-        public void IdnHost_NntpFqdn_Success()
-        {
-            CommonSchemesTest("nntp");
-        }
-
-        [Fact]
-        public void IdnHost_TelnetFqdn_Success()
-        {
-            CommonSchemesTest("telnet");
-        }
-
-        [Fact]
-        public void IdnHost_LdapFqdn_Success()
-        {
-            CommonSchemesTest("ldap");
-        }
-
-        [Fact]
-        public void IdnHost_Internal_Call_ThrowsException()
-        {
-            Assert.Throws<InvalidOperationException>(() =>
-          {
-              Uri u = new Uri("/test/test2/file.tst", UriKind.Relative);
-              string s = u.DnsSafeHost;
-          });
-        }
-
-        #region Helpers
-
-        public void CommonSchemesTest(string scheme)
+        public static IEnumerable<object[]> CommonSchemes_Data()
         {
             string unicodeHost = "a\u00FChost.dom\u00FCin.n\u00FCet";
             string punycodeHost = "xn--ahost-kva.xn--domin-mva.xn--net-hoa";
 
-            // initial unicode host
-            ValidateUri(scheme, unicodeHost, UriHostNameType.Dns, unicodeHost, unicodeHost, punycodeHost);
-
-            // initial punycode host
-            ValidateUri(scheme, punycodeHost, UriHostNameType.Dns, punycodeHost, punycodeHost, punycodeHost);
+            foreach (string scheme in new[] {
+                "unknown",
+                "http",
+                "ftp",
+                "file",
+                "net.pipe",
+                "net.tcp",
+                "vsmacros",
+                "gopher",
+                "nntp",
+                "telnet",
+                "ldap",
+            })
+            {
+                yield return new object[] { scheme, unicodeHost, unicodeHost, unicodeHost, punycodeHost };
+                yield return new object[] { scheme, punycodeHost, punycodeHost, punycodeHost, punycodeHost };
+            }
         }
 
-        public void ValidateUri(
+        [Theory]
+        [MemberData(nameof(CommonSchemes_Data))]
+        public static void CommonSchemes_Test(
             string scheme,
             string host,
-            UriHostNameType expectedHostType,
             string expectedHost,
             string expectedDnsSafeHost,
             string expectedIdnHost)
@@ -127,11 +67,9 @@ namespace System.PrivateUri.Tests
             Assert.True(Uri.TryCreate(scheme + "://" + host, UriKind.Absolute, out uri));
             Assert.Equal(expectedHost, uri.Host);
             Assert.Equal(expectedDnsSafeHost, uri.DnsSafeHost);
-            Assert.Equal(expectedHostType, uri.HostNameType);
-            Assert.Equal(expectedHostType, Uri.CheckHostName(host));
+            Assert.Equal(UriHostNameType.Dns, uri.HostNameType);
+            Assert.Equal(UriHostNameType.Dns, Uri.CheckHostName(host));
             Assert.Equal(expectedIdnHost, uri.IdnHost);
         }
-
-        #endregion Helpers
     }
 }
